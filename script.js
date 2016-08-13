42a34e02c4e23b4d09e7be9ffb89c462a9ed1f04
Reuven Karasik <rubik@karasik.org>
2016-08-13 18:33:40 +0300

    Added lots of functionality

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var mouse = [0,0];
+var mouse = [0,0],
+	foods = [];
+
+function getPixelValue(value, type) {
+	var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(type)]];
+	return side * (value/100);
+}
 $(function() {
 
 	document.onmousemove = handleMouseMove;
@@ -31,13 +37,47 @@ $(function() {
 
 	}
 
+	addFood();
+
 
 });
 
+
+
 function Fish() {
 	this.position = [0,0];
+	this.size = 50;
 	this.element = $('#fish');
+	this.updateSize();
+
+	setInterval(function() {
+		foods.forEach(function(food) {
+			if(fish.isTouchingFood(food)) {
+				food.element.addClass('being-eaten');
+				setTimeout(function () {
+					if(fish.isTouchingFood(food) && $.contains(document, food.element[0]))fish.eatFood(food);
+					else food.element.removeClass('being-eaten');
+				}, 1000);
+			}
+		});
+
+	}, 100);
 }
+
+Fish.prototype.isTouchingFood = function(food) {
+	return (Math.abs(this.position[1] - getPixelValue(food.position[0], 'vw')) < 20)
+		&& (Math.abs(this.position[0] - getPixelValue(food.position[1], 'vh')) < 20);
+};
+
+Fish.prototype.updateSize = function() {
+	this.element.css('height', this.size);
+};
+
+Fish.prototype.eatFood = function (food) {
+	food.destroy();
+	this.size+=4;
+	this.updateSize();
+};
 
 Fish.prototype.turnToPointer = function () {
 	var deltaX = mouse[0] - this.position[0];
@@ -60,6 +100,19 @@ Fish.prototype.moveTo = function (x,y) {
 var fish = new Fish();
 
 
+function addFood() {
+	var theFood = new Food();
+
+	var timer = (Math.random()*3);
+
+	// Add food in timer seconds
+	setTimeout(addFood, timer*1000);
+
+	// Destroy the food after 8 seconds
+	setTimeout(function(){
+		theFood.destroy();
+	}, 4000);
+}
 
 function Food() {
 
@@ -69,6 +122,8 @@ function Food() {
 	this.setRandomPosition();
 
 	$('#container').append(this.element);
+
+	foods.push(this);
 }
 
 Food.prototype.setRandomPosition = function () {
@@ -76,11 +131,16 @@ Food.prototype.setRandomPosition = function () {
 		y = Math.floor(Math.random()*100);
 	this.position = [x,y];
 	this.element.css({
-		top: this.position[0] + 'vh',
-		left: this.position[1] + 'vw'
+		top: this.position[0] + 'vw',
+		left: this.position[1] + 'vh'
 	});
 };
 
 Food.prototype.destroy = function() {
 	this.element.remove();
+
+	// Find the index of this food and remove it from the foods array
+	var index = foods.indexOf(this);
+	if(index > -1)
+		foods.splice(index,1);
 };
